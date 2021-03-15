5e5890a43e23a20be47440072242af01fc3d2ca5
tarcisiorr <tarcisio@gmail.com>
2021-03-15 18:16:03 -0300

    Curso de Node, projeto 2, aula 43

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ app.get("/", (req, res) => {
             ['id', 'DESC']
         ]
     }).then(articles => {
-        res.render("index", {articles: articles});
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories});
+        });
     });
 });
 
@@ -51,7 +53,9 @@ app.get("/:slug", (req, res) => {
         }
     }).then(article => {
         if(article != undefined){
-            res.render("article", {article: article});
+            Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+            })
         }else{
             res.redirect("/");
         }
@@ -59,6 +63,26 @@ app.get("/:slug", (req, res) => {
         res.redirect("/");
     });
 });
+
+app.get("/category/:slug",(req, res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then(category => {
+        if (category != undefined){
+            Category.findAll().then(categories => {
+                res.render("index",{articles: category.articles, categories: categories});
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch ( err => {
+        res.redirect("/");
+    })
+})
 
 app.listen(8080, () => {
     console.log("O servidor está rodando!");
